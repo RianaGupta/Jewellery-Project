6751a7aa7790321626df7b8af84d6cd6e3d01e45
Riana Gupta <riana.rg0307@gmail.com>
2026-08-15 11:38:10 +0530

    Updated project

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import Purse from './pages/purse'
 import Category from './pages/Category'
 import { Routes, Route } from 'react-router-dom'
 import About from './pages/About'
-import Style from './pages/style'
+// import Style from './pages/style'
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/Style" element={<Style />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/Style" element={<Style />} /> */}
 
         <Route path="/purse" element={<Purse />} />
-         <Route
+        <Route
           path="/category/:category"
-          element={<Category/>}
+          element={<Category />}
         />
       </Routes>
 
